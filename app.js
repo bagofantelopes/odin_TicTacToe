cad91429f6d02f424281c7b2ff1c00057716d1eb
Travis F <agathangalos@gmail.com>
2022-01-14 12:43:19 -0500

    initial commit

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
-const Gameboard = () => {
-    let count = 0;
-    let boardSpaces = [
-    '[]','[]','[]',
-    '[]','[]','[]',
-    '[]','[]','[]'];
+const Gameboard = (() => {
+    let turnCounter = 0;
+    let board = document.querySelectorAll('.board_square');
+    let board2 = [];
+    board2.length = 9;
+    board2.fill('test', 0, 8);
+
+
+    board.forEach((square, index) => {
+        square.addEventListener('click', (e) => {
+            let p = document.createElement('p')
+            if (turnCounter == 0) {
+                p.innerText = 'x';
+                p.setAttribute('class', 'player_move_x');
+                turnCounter = 1;
+            } else if (turnCounter == 1) {
+                p.innerText = 'o';
+                p.setAttribute('class', 'player_move_o');
+                turnCounter = 0;
+            }
+            square.appendChild(p);
+        })
+    })
     const makeBoard = () => {
-        console.log(boardSpaces);
+        console.log(board2);
     }
     return { makeBoard };
-}
+})();
 
 const Player = (name, symbol) => {
     const getName = () => name;
@@ -19,10 +36,15 @@ const Player = (name, symbol) => {
     };
 
     return {getPlayerInfo};
-}
+};
+
+const Gameplay = (player1, player2) => {
+
+};  
 
 let jimmy = Player('jimmy', 'x');
+let bobby = Player('bobby', 'o');
 jimmy.getPlayerInfo();
 
-let board1 = Gameboard();
-board1.makeBoard();
+
+Gameboard.makeBoard();
