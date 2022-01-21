01ba14586e40595bf52d3014432dddc9e33b8907
Travis F <agathangalos@gmail.com>
2022-01-21 15:11:50 -0500

    Added proper detection for ties and cleaned up the algorithm to detect wins a little. Cleaned up the GUI into a mostly final state.

--- app.js
+++ app.js
@@ -35,11 +35,9 @@ const gameBoard = (() => {
             };
         });
         document.querySelector('#win_panel').style.display = 'none';
-        // add the main game board to the DOM
-        document.querySelector('#main').appendChild(main_board);
+        document.querySelector('#main').appendChild(main_board); // add the main game board to the DOM
         document.getElementById('main').style.display = 'none';
     };
-
     return { board, makeBoard };
 })();
 
@@ -49,13 +47,14 @@ const displayController = (() => {
 
     // sets up the initial game state to be ready for play
     // @param player1, player2 The Player objects
-    const gameSetup = (player1, player2) => {
+    const gameSetup = (player1, player2, moves) => {
 
         player1.setSymbol('X');
         player2.setSymbol('O');
 
-        // event handler for 
+        // event handler for the button to start the whole game!
         document.getElementById('game_start').addEventListener('click', e => {
+            playerMoves(player1, player2, gameBoard.board);
             document.getElementById('game_start').style.display = 'none';
             playerOneName = document.getElementById('player_1_name').value;
             if (!playerOneName) {
@@ -72,13 +71,10 @@ const displayController = (() => {
             } else {
                 player2.setName(playerTwoName);
             }
-            
             document.getElementById('player_2_label').innerText = `${player2.getName()}`;
             document.getElementById('player_2_name').style.display = 'none';
 
-
-            // show the game board!
-            document.getElementById('main').style.display = 'block';
+            document.getElementById('main').style.display = 'block'; // show the game board!
 
             let playerOneStats = document.createElement('p')
             playerOneStats.setAttribute('id', 'player_one_stats');
@@ -91,24 +87,20 @@ const displayController = (() => {
             document.querySelector('#player_2_panel').appendChild(playerTwoStats);
         });
 
-
-
         // RESET GAME FUNCTION
         let reset = document.getElementById('reset_button');
         reset.addEventListener('click', e => {
-
             gameBoard.board.fill('',0, 9);
             let nodelist = document.querySelectorAll('#main #game_board .board_row .board_square')
             nodelist.forEach((square) => {
                 square.innerText = '';
+                square.style.pointerEvents = 'fill';
             });
             gameLogic.reset();
         })
-
     };
 
     // a simple bool to occilate between the player turns
-    // is there maybe a better way to handle this? Fine for now
     player1_turn = true;
     
     // event handlers and logic for players clicking on the gameboard
@@ -116,7 +108,6 @@ const displayController = (() => {
     // appends the move to the "moves" array as well
     // @param player1, player2 The two Player objects 
     // @param moves The board array from gameBoard.board module; 
-    //        player moves are added to it and the array is passed to gameLogic.detectWinner with each move
     const playerMoves = (player1, player2, moves) => {
         let nodelist = document.querySelectorAll('#main #game_board .board_row .board_square')
         //console.log(nodelist);
@@ -142,7 +133,6 @@ const displayController = (() => {
             });
         });
     };
-
     return { gameSetup, playerMoves }
 })();
 
@@ -155,62 +145,52 @@ const gameLogic = (() => {
     const reset = () => {
         moves_remaining = 9;
         document.querySelector('#win_panel').style.display = 'none';
-        console.log(moves_remaining);
     };
 
     // checks for a winner of the game each time a move is made
-    // @param arr The array of moves so far, still originally the board array from gameBoard.board module
+    // @param arr The array of moves made so far
     // @param player The player who just made a move
-    // @param e The last square that was clicked on
+    // @param e The last square that was clicked on`
     const detectWinner = (e, arr, player) => {
 
-        // check rows for win
-        // i+=3 so it doesn't erroneously look at sequential (array)
-        // indices that appear in different 'rows' of the gameboard as a win
-        for (i = 0; i < arr.length; i+=3) {
-            if (arr[i] == '')
-                continue;
-            if (arr[i] == arr[i+1] && arr[i] == arr[i+2]){
-                game_end_events.handleEvent(e, player)
-                break;            
-            }
-        }
-
-        // check columns for win
-        // no index oddities like with the rows were noticed so just iterate through normally
+        // to flag a tie when combined with moves_remaining
+        let win = false;
         for (i = 0; i < arr.length; i++) {
+            // check columns for win
+            // no index oddities like with the rows were noticed so just iterate through normally
             if (arr[i] == '')
                 continue;
             if (arr[i] == arr[i+3] && arr[i] == arr[i+6]) {
-                game_end_events.handleEvent(e, player)
+                win = true;
+                game_end_events.handleWin(e, player, win)
+                break;
+            
+            // check rows for wins
+            // Unlike with columns just iterating through the array will detect sequential squares
+            // in different rows and erroneously report a win
+            // so the exta code restricts the check to the start of each row (indices 0, 3, 6)
+            } else if ((i == 0 || i%3 == 0) && arr[i] == arr[i+2] && arr[i+1] == arr[i+2]) {
+                win = true;
+                game_end_events.handleWin(e, player, win);
                 break;
                 
-            // diagonals prove tricky, most likely because the logic of how I 
-            // setup the gameboard is flawed somewhere (or the way of doing things is really dumb)
-            // seems to work without error though
-
-            // detects a 'top-left' to 'bottom-right'
+            // diagonals had a similar problem to rows, but a simple clause
+            // to restrict the initial indice checked keeps things working
             } else if (i == 0 && arr[0] == arr[i+4] && arr[i+4] == arr[i+8]) {
-                game_end_events.handleEvent(e, player)
+                win = true;
+                game_end_events.handleWin(e, player, win)
                 break;
-
-            // detects a 'top-right' to 'bottom-left'
             } else if (i == 2 && arr[2] == arr[4] && arr[4] == arr[6]) {
-                game_end_events.handleEvent(e, player)
+                win = true;
+                game_end_events.handleWin(e, player, win)
                 break;
             }
         }
-
-        // This should detect and fire a tie game in most instances
-        // currently if the last move affects a win, this will still fire,
-        // but once we move the win prompts to the DOM that shouldn't be an issue anymore
         moves_remaining--;
-        //console.log(moves_remaining);
-        if (moves_remaining <= 0) {
-            alert('TIE GAME');
+        if (moves_remaining <= 0 && win == false) {
+            game_end_events.handleTie(e, win);
         }
     };
-
 
     let win_panel = document.querySelector('#win_panel')
     let p = document.createElement('p');
@@ -219,36 +199,32 @@ const gameLogic = (() => {
     // object to handle the various game end states
     const game_end_events = {
     
-        handleEvent(e, player) {
-            switch(e.type) {
-                case 'click':
-                    this.handlers.click(e, player);
+        handleWin(e, player) {
+            let nodelist = document.querySelectorAll('#main #game_board .board_row .board_square')
+            nodelist.forEach(square => {
+                square.style.pointerEvents = 'none'; // disable clicks since game is over
+            });
+            player.setWins();
+            win_panel.style.display = 'flex';
+            if(player.getSymbol() == 'X') {
+                document.querySelector('#player_one_stats').innerText = 
+                    `Games Won: ${player.getWins()}`;
+
+                p.innerText = `Victory for ${player.getName()}!`
+            } else if(player.getSymbol() == 'O') {
+                document.querySelector('#player_two_stats').innerText = 
+                    `Games Won: ${player.getWins()}`;
+
+                p.innerText = `Victory for ${player.getName()}!`
             }
         },
 
-        handlers: {
-            click(e, player) {
-                player.setWins();
-                win_panel.style.display = 'flex';
-                console.log(`victory for ${player.getName()} yayyy!`)
-                if(player.getSymbol() == 'X') {
-                    document.querySelector('#player_one_stats').innerText = 
-                        `Games Won: ${player.getWins()}`;
+        handleTie(e, win) {
+            alert('SHIT IS TIED YO')
 
-                    p.innerText = `Victory for ${player.getName()}!`
-                } else if(player.getSymbol() == 'O') {
-                    document.querySelector('#player_two_stats').innerText = 
-                        `Games Won: ${player.getWins()}`;
-
-                    p.innerText = `Victory for ${player.getName()}!`
-                }
-    
-            },
         },
     }
-
     return { detectWinner, reset };
-
 })();
 
 // factory function to make Player objects
@@ -267,7 +243,6 @@ const Player = () => {
     const setWins = () => wins++;
     const getWins = () => wins;
 
-
     return {
         getName, setName, 
         getSymbol, setSymbol,
@@ -275,11 +250,9 @@ const Player = () => {
     };
 };
 
-
-
 let player1 = Player();
 let player2 = Player();
 
 gameBoard.makeBoard();
-displayController.gameSetup(player1, player2);
-displayController.playerMoves(player1, player2, gameBoard.board);
+displayController.gameSetup(player1, player2, gameBoard.board);
+//displayController.playerMoves(player1, player2, gameBoard.board);
